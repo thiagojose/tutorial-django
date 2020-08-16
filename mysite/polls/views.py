4ad99ddcc9a01d7f8598093d4a89cc6ccfb0ee39
thiagojose <thiagojose@thiagodfaith@gmail.com>
2020-08-16 18:58:55 -0300

    testando ssh

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -10,6 +10,7 @@ from django.utils import timezone
 
 from .models import Question, Choice
 
+
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
